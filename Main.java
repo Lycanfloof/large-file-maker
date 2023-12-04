f4a5f050242575cd627f45c0bb398bac24c046be
lycanfloof <lyca2206@gmail.com>
2023-12-03 20:03:54 -0500

    Implemented the code.

---
+++ Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Main {
+    private static final int ONE_GIB = 1073741824;
+    private static final int CHUNK = 134217728;
+    private static final  char[] symbolArray = initializeSymbolArray();
+
+    private static char[] initializeSymbolArray() {
+        char[] symbols = new char[26 * 2 + 10];
+        int index = 0;
+
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            symbols[index++] = ch;
+        }
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            symbols[index++] = ch;
+        }
+
+        for (char ch = '0'; ch <= '9'; ch++) {
+            symbols[index++] = ch;
+        }
+
+        return symbols;
+    }
+
+    public static void main(String[] args) {
+        int size;
+
+        if (args.length <= 0) {
+            size = ONE_GIB;
+        } else {
+            size = ONE_GIB * Integer.valueOf(args[0]);
+        }
+
+        try {
+            createFile(size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createFile(int size) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("unsorted_file"));
+
+        int chunks = (int) Math.ceil(size / CHUNK);
+        for (int i = 0; i < chunks; i++) {
+            bw.write(createChunk());
+        }
+
+        bw.close();
+    }
+
+    private static char[] createChunk() {
+        char[] chunk = new char[CHUNK];
+        int length = (int) (Math.random() * 12 + 4);
+
+        for (int i = 0; i < CHUNK; i++) {
+            if (length <= 0) {
+                chunk[i] = '\n';
+                length = (int) (Math.random() * 12 + 4);
+            } else {
+                int random = (int) (Math.random() * 35);
+                chunk[i] = symbolArray[random];
+                length--;
+            }
+        }
+
+        return chunk;
+    }
+}
